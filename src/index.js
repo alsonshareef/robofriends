56ec61cf7aac9e42d1cf3831b6ad7ab3d3527db0
Alson Shareef <alson.shareef@gmail.com>
2018-10-19 19:04:03 +0800

    Built 'Cardlist' component to take in data and create cards with it

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import Cardlist from './Cardlist';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import {robots} from './robots';
 
-ReactDOM.render(<Card 
-                    id={robots[0].id} 
-                    name={robots[0].name} 
-                    username={robots[0].username} 
-                    email={robots[0].email}
-                />,
-                document.getElementById('root'));
+ReactDOM.render(<Cardlist robots={robots}/>,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
