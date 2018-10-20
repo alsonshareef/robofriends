1d9eb7d155fe258e2747b35f72646377f17759ae
Alson Shareef <alson.shareef@gmail.com>
2018-10-20 18:58:31 +0800

    Added componentDidMount to update state of robots

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Cardlist from './Cardlist';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
 
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}))
     }
 
     onSearchChange = (event) => {
@@ -21,13 +26,17 @@ class App extends React.Component {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
         
-        return (
-            <div className='tc'>
-                <h1 className='f3'>ROBOFRIENDS</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <Cardlist robots={filteredRobots} />
-            </div>
-        )
+        if (this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f3'>ROBOFRIENDS</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Cardlist robots={filteredRobots} />
+                </div>
+            )
+        }
     }
 }
 
